816ecf55daf7db5b2490e4935dc1ca077c4fafc7
Lekaha <lekaha@gmail.com>
2013-01-23 16:22:22 +0800

    Update samples/epg/src/fr/julienvermet/twodadapter/sample/epg/MainActivity.java
    Test skip a program.

--- samples/epg/src/fr/julienvermet/twodadapter/sample/epg/MainActivity.java
+++ samples/epg/src/fr/julienvermet/twodadapter/sample/epg/MainActivity.java
@@ -60,15 +60,32 @@ public class MainActivity extends Activity {
 		
 		for (int i=0; i<50; i++) {
 			ArrayList<TvProgram> channel = new ArrayList<TvProgram>();
+			
 			TvProgram tvProgram1 = new TvProgram(1356631200, 1356634800, "Program"+ (i+1));
-			TvProgram tvProgram2 = new TvProgram(1356635000, 1356645400, "Program2"+ (i+2));
-			TvProgram tvProgram3 = new TvProgram(1356645800, 1356655800, "Program3"+ (i+3));
-			TvProgram tvProgram4 = new TvProgram(1356657800, 1356667800, "Program4"+ (i+4));
+			TvProgram tvProgram2 = new TvProgram(1356634800, 1356638400, "Program2"+ (i+2));
+			TvProgram tvProgram3 = new TvProgram(1356638400, 1356642000, "Program3"+ (i+3));
+			TvProgram tvProgram4 = new TvProgram(1356642000, 1356645600, "Program4"+ (i+4));
+			TvProgram tvProgram5 = new TvProgram(1356645600, 1356649200, "Program5"+ (i+5));
+			TvProgram tvProgram6 = new TvProgram(1356649200, 1356652800, "Program6"+ (i+6));
+			TvProgram tvProgram7 = new TvProgram(1356652800, 1356656400, "Program7"+ (i+7));
+			TvProgram tvProgram8 = new TvProgram(1356656400, 1356660000, "Program8"+ (i+8));
+			TvProgram tvProgram9 = new TvProgram(1356660000, 1356663600, "Program9"+ (i+9));
+			TvProgram tvProgram10 = new TvProgram(1356663600, 1356667200, "Program10"+ (i+10));
+			
 			channel.add(tvProgram1);
 			channel.add(tvProgram2);
 			channel.add(tvProgram3);
 			channel.add(tvProgram4);
-			mTvChannels.put(i, channel);
+			channel.add(tvProgram5);
+			channel.add(tvProgram6);
+			channel.add(tvProgram7);
+			channel.add(tvProgram8);
+			channel.add(tvProgram9);
+			channel.add(tvProgram10);
+			
+			if(i != 2){
+				mTvChannels.put(i, channel);
+			}
 		}
 
 		new TwoDContentAdapter(mTwoDScrollView, mTwoDContent);
@@ -79,10 +96,11 @@ public class MainActivity extends Activity {
 		int channelY = 0;
 		
 		for (int i=0; i<mTvChannels.size(); i++) {
+			int index = mTvChannels.indexOfValue(mTvChannels.get(i));
 			if ((channelY >= y1 && channelY <= y2) || (channelY+mEpgElementHeight >= y1 && channelY+mEpgElementHeight <= y2)) {
-				visibleChannels.add(i);
+				visibleChannels.add(index);
 			}
-			channelY += mEpgElementHeight;
+			channelY = index * (int) mEpgElementHeight;
 		}
 		return visibleChannels;
 	}
@@ -120,16 +138,17 @@ public class MainActivity extends Activity {
 				float scrollRight, float scrollTop, float scrollBottom) {
 
 			ArrayList<TwoDElement<TvProgram>> mElements = new ArrayList<TwoDElement<TvProgram>>();
-
 			ArrayList<Integer> visibleChannels = getVisibleChannels(scrollTop, scrollBottom);
 			for (int channel : visibleChannels) {
 				float channelY = (int) (channel * mEpgElementHeight);
 				ArrayList<TvProgram> tvPrograms = mTvChannels.get(channel);
-				for (TvProgram tvProgram : tvPrograms) {
-					float programX = getProgramX(tvProgram.startTime);
-					float programWidth = getProgramWidth(tvProgram.startTime, tvProgram.endTime);
-					TwoDElement<TvProgram> element = new TwoDElement<TvProgram>(programX, channelY, programWidth, mEpgElementHeight, tvProgram);
-					mElements.add(element);
+				if(null != tvPrograms){
+					for (TvProgram tvProgram : tvPrograms) {
+						float programX = getProgramX(tvProgram.startTime);
+						float programWidth = getProgramWidth(tvProgram.startTime, tvProgram.endTime);
+						TwoDElement<TvProgram> element = new TwoDElement<TvProgram>(programX, channelY, programWidth, mEpgElementHeight, tvProgram);
+						mElements.add(element);
+					}
 				}
 			}
 			return mElements;
